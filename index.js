cf587d6ee7294e8ffc4e32caad35486ca77e6d90
HunterXuan <endpot@gmail.com>
2019-07-27 18:35:46 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var util = require('hexo-util')
-var version = '0.1.0'
+var version = '0.2.0'
 
 hexo.extend.filter.register('after_post_render', function(data) {
   var htmlTags = ''
@@ -7,20 +7,21 @@ hexo.extend.filter.register('after_post_render', function(data) {
   // kity.js
   htmlTags = htmlTags + util.htmlTag('script', {
     type: 'text/javascript',
-    href: 'https://cdn.jsdelivr.net/npm/kity@2.0.4/dist/kity.min.js'
-  })
+    src: 'https://cdn.jsdelivr.net/npm/kity@2.0.4/dist/kity.min.js'
+  }, '')
 
   // kityminder-core.js
   htmlTags = htmlTags + util.htmlTag('script', {
     type: 'text/javascript',
-    href: 'https://cdn.jsdelivr.net/npm/kityminder-core@1.4.50/dist/kityminder.core.min.js'
-  })
-  
+    src: 'https://cdn.jsdelivr.net/npm/kityminder-core@1.4.50/dist/kityminder.core.min.js'
+  }, '')
+
   // mindmap.js
   htmlTags = htmlTags + util.htmlTag('script', {
+	defer: 'true',
     type: 'text/javascript',
     src: 'https://cdn.jsdelivr.net/npm/hexo-simple-mindmap@' + version + '/dist/mindmap.min.js'
-  })
+  }, '')
 
   // mindmap.css
   htmlTags = htmlTags + util.htmlTag('link', {
