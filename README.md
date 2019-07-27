d3d838eb5114aa5410a8295f1af999cac78d406d
HunterXuan <endpot@gmail.com>
2019-07-27 17:16:32 +0800

    init version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hexo-mindmap
 Hexo Plugin: Display mindmap in post
+
+# How to use
+
+## Install
+```bash
+npm install hexo-mindmap
+```
+
+## Tag Example
+```
+{% pullquote mindmap mindmap-md %}
+- [在 Hexo 中使用思维导图](https://hunterx.xyz/use-mindmap-in-hexo.html)
+  - 前言
+  - 操作指南
+    - 准备需要的文件
+    - 为主题添加 CSS/JS 文件
+  - 使用方法
+{% endpullquote %}
+```
+
+## Further Reading
+[在 Hexo 中使用思维导图](https://hunterx.xyz/use-mindmap-in-hexo.html)
---
+++ index.js
@@ -0,0 +1,37 @@
+var util = require('hexo-util')
+var version = '0.1.0'
+
+hexo.extend.filter.register('after_post_render', function(data) {
+  var htmlTags = ''
+
+  // kity.js
+  htmlTags = htmlTags + util.htmlTag('script', {
+    type: 'text/javascript',
+    href: 'https://cdn.jsdelivr.net/npm/kity@2.0.4/dist/kity.min.js'
+  })
+
+  // kityminder-core.js
+  htmlTags = htmlTags + util.htmlTag('script', {
+    type: 'text/javascript',
+    href: 'https://cdn.jsdelivr.net/npm/kityminder-core@1.4.50/dist/kityminder.core.min.js'
+  })
+  
+  // mindmap.js
+  htmlTags = htmlTags + util.htmlTag('script', {
+    type: 'text/javascript',
+    src: 'https://cdn.jsdelivr.net/npm/hexo-mindmap@' + version + '/dist/mindmap.min.js'
+  })
+
+  // mindmap.css
+  htmlTags = htmlTags + util.htmlTag('link', {
+    rel: 'stylesheet',
+    type: 'text/css',
+    href: 'https://cdn.jsdelivr.net/npm/hexo-mindmap@' + version + '/dist/mindmap.min.css'
+  })
+
+  data.content = data.content + htmlTags
+  
+  console.log(htmlTags)
+
+  return data
+});
