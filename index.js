c97b990f2b0ab4ea1e30a89fdbed9877b6a572c1
HunterX <endpot@gmail.com>
2022-01-12 14:37:33 +0000

    feat: replace jsdelivr

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ hexo.extend.filter.register('after_post_render', function(data) {
   // kity.js
   htmlTags = htmlTags + util.htmlTag('script', {
     type: 'text/javascript',
-    src: 'https://cdn.jsdelivr.net/npm/kity@2.0.4/dist/kity.min.js'
+    src: 'https://unpkg.com/kity@2.0.4/dist/kity.min.js'
   }, '')
 
   // kityminder-core.js
   htmlTags = htmlTags + util.htmlTag('script', {
     type: 'text/javascript',
-    src: 'https://cdn.jsdelivr.net/npm/kityminder-core@1.4.50/dist/kityminder.core.min.js'
+    src: 'https://unpkg.com/kityminder-core@1.4.50/dist/kityminder.core.min.js'
   }, '')
 
   // mindmap.js
   htmlTags = htmlTags + util.htmlTag('script', {
 	defer: 'true',
     type: 'text/javascript',
-    src: 'https://cdn.jsdelivr.net/npm/hexo-simple-mindmap@' + version + '/dist/mindmap.min.js'
+    src: 'https://unpkg.com/hexo-simple-mindmap@' + version + '/dist/mindmap.min.js'
   }, '')
 
   // mindmap.css
   htmlTags = htmlTags + util.htmlTag('link', {
     rel: 'stylesheet',
     type: 'text/css',
-    href: 'https://cdn.jsdelivr.net/npm/hexo-simple-mindmap@' + version + '/dist/mindmap.min.css'
+    href: 'https://unpkg.com/hexo-simple-mindmap@' + version + '/dist/mindmap.min.css'
   })
 
   data.content = data.content + htmlTags
